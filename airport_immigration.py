7537089f60a33b9c7a7408ee12c94f80e81ce28b
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:48:32 +0500

    Add files via upload

---
+++ airport_immigration.py
@@ -0,0 +1,22 @@
+print("===== IMMIGRATION =====")
+
+passport = input("Passport (yes/no): ").lower()
+visa = input("Visa (yes/no): ").lower()
+ticket = input("Ticket (yes/no): ").lower()
+
+if passport == "yes":
+
+    if visa == "yes":
+
+        if ticket == "yes":
+
+            print("Immigration Cleared")
+
+        else:
+            print("Ticket Missing")
+
+    else:
+        print("Visa Missing")
+
+else:
+    print("Passport Missing")
