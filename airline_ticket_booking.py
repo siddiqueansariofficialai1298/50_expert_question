44fe61b85cf5e5bb7191acf8f313da9ac724b396
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:42:49 +0500

    Add files via upload

---
+++ airline_ticket_booking.py
@@ -0,0 +1,18 @@
+print("===== Airline Booking =====")
+
+ticket = 30000
+
+class_type = input("Class (economy/business): ").lower()
+baggage = float(input("Baggage (kg): "))
+meal = input("Meal Required (yes/no): ").lower()
+
+if class_type == "business":
+    ticket += 20000
+
+if baggage > 20:
+    ticket += (baggage - 20) * 1000
+
+if meal == "yes":
+    ticket += 1500
+
+print("Total Ticket Price:", ticket)
