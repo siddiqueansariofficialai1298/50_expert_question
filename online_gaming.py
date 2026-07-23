4700a5bd7188830e118f43a5ef6ca87bea6db0d5
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:34:36 +0500

    Add files via upload

---
+++ online_gaming.py
@@ -0,0 +1,16 @@
+print("===== Gaming Login =====")
+
+username = input("Username: ")
+password = input("Password: ")
+banned = input("Account Banned (yes/no): ").lower()
+
+if username == "admin":
+    if password == "game123":
+        if banned == "no":
+            print("Login Successful")
+        else:
+            print("Account Banned")
+    else:
+        print("Wrong Password")
+else:
+    print("Username Not Found")
