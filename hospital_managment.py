d6b5d3883a17e1246d1df465af7a6cf23baaa511
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:45:58 +0500

    Add files via upload

---
+++ hospital_managment.py
@@ -0,0 +1,23 @@
+print("===== HOSPITAL MANAGEMENT =====")
+
+age = int(input("Age: "))
+emergency = input("Emergency (yes/no): ").lower()
+insurance = input("Insurance (yes/no): ").lower()
+
+if emergency == "yes":
+    priority = "HIGH"
+
+elif age >= 60:
+    priority = "HIGH"
+
+else:
+    priority = "NORMAL"
+
+print("Priority =", priority)
+
+bill = 50000
+
+if insurance == "yes":
+    bill -= bill * 0.20
+
+print("Bill =", bill)
