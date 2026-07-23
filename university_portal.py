28720a699a61d895f19171608b83a9c4ffa6ab54
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 16:06:37 +0500

    Add files via upload

---
+++ university_portal.py
@@ -0,0 +1,22 @@
+print("===== UNIVERSITY ADMISSION =====")
+
+inter = float(input("Intermediate %: "))
+entry = float(input("Entry Test %: "))
+interview = input("Interview (pass/fail): ").lower()
+
+if inter >= 70:
+
+    if entry >= 75:
+
+        if interview == "pass":
+
+            print("Admission Confirmed")
+
+        else:
+            print("Interview Failed")
+
+    else:
+        print("Entry Test Failed")
+
+else:
+    print("Intermediate Marks Low")
