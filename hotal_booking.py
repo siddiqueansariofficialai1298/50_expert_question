b82a1a4cdc65518b573b029a14ff34c16151d028
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 14:19:15 +0500

    Add files via upload

---
+++ hotal_booking.py
@@ -0,0 +1,18 @@
+print("===== Hotel Booking =====")
+
+days = int(input("Days: "))
+room = input("Room (standard/deluxe): ").lower()
+
+if room == "standard":
+    bill = days * 3000
+elif room == "deluxe":
+    bill = days * 6000
+else:
+    bill = 0
+
+coupon = input("Coupon Available (yes/no): ").lower()
+
+if coupon == "yes":
+    bill *= 0.9
+
+print("Total Bill =", bill)
