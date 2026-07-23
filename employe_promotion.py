6444efd007ba5ae7cd41ae7d3e78b2fd18c31833
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 14:07:40 +0500

    Add files via upload

---
+++ employe_promotion.py
@@ -0,0 +1,16 @@
+print("===== Employee Promotion =====")
+
+experience = int(input("Experience (Years): "))
+performance = int(input("Performance Score: "))
+attendance = int(input("Attendance (%): "))
+
+if experience >= 5:
+    if performance >= 90:
+        if attendance >= 95:
+            print("🎉 Promotion Approved")
+        else:
+            print("❌ Attendance Too Low")
+    else:
+        print("❌ Performance Too Low")
+else:
+    print("❌ Experience Too Low")
