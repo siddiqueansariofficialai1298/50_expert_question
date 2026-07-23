092e1e371718103739607a6c4e899dbf2424f606
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:16:52 +0500

    Add files via upload

---
+++ university_hostal.py
@@ -0,0 +1,16 @@
+print("===== Hostel Allotment =====")
+
+merit = float(input("Merit Percentage: "))
+distance = int(input("Distance from University (KM): "))
+fee = input("Fee Paid (yes/no): ").lower()
+
+if merit >= 75:
+    if distance >= 50:
+        if fee == "yes":
+            print("Hostel Allotted")
+        else:
+            print("Hostel Fee Pending")
+    else:
+        print("Distance Too Short")
+else:
+    print("Merit Too Low")
