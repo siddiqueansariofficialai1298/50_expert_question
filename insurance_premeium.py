37fbb594ccd0e0f2c2af854caf188f8e235d518c
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 14:16:16 +0500

    Add files via upload

---
+++ insurance_premeium.py
@@ -0,0 +1,20 @@
+print("===== Insurance Premium =====")
+
+age = int(input("Age: "))
+smoker = input("Smoker (yes/no): ").lower()
+disease = input("Heart Disease (yes/no): ").lower()
+
+if age < 30:
+    premium = 1000
+elif age < 50:
+    premium = 2000
+else:
+    premium = 4000
+
+if smoker == "yes":
+    premium += 1500
+
+if disease == "yes":
+    premium += 2000
+
+print("Total Premium =", premium)
