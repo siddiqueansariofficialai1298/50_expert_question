8673a510751712cea2ec7ca1e351d8e9b7537064
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 14:48:03 +0500

    Add files via upload

---
+++ creadit_card_approval.py
@@ -0,0 +1,10 @@
+print("===== Credit Card Approval =====")
+
+salary = int(input("Salary: "))
+credit = int(input("Credit Score: "))
+loan = input("Existing Loan (yes/no): ").lower()
+
+if salary >= 100000 and credit >= 750 and loan == "no":
+    print("Credit Card Approved")
+else:
+    print("Credit Card Rejected")
