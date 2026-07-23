c1bffb7c53295cdf419d56b5d401eff4c46bd384
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 14:12:27 +0500

    Add files via upload

---
+++ Driving_license.py
@@ -0,0 +1,20 @@
+print("===== Driving License =====")
+
+age = int(input("Age: "))
+eye = input("Eye Test (pass/fail): ").lower()
+written = input("Written Test (pass/fail): ").lower()
+road = input("Road Test (pass/fail): ").lower()
+
+if age >= 18:
+    if eye == "pass":
+        if written == "pass":
+            if road == "pass":
+                print("✅ License Approved")
+            else:
+                print("❌ Road Test Failed")
+        else:
+            print("❌ Written Test Failed")
+    else:
+        print("❌ Eye Test Failed")
+else:
+    print("❌ Under Age")
