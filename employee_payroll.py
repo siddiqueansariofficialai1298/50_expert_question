4159ce74bb0ff707663d1bb6f175709d2ca15ee2
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:33:06 +0500

    Add files via upload

---
+++ employee_payroll.py
@@ -0,0 +1,16 @@
+print("===== Employee Payroll =====")
+
+salary = float(input("Basic Salary: "))
+overtime = int(input("Overtime Hours: "))
+loan = float(input("Loan Deduction: "))
+
+total = salary + (overtime * 500)
+
+if total > 100000:
+    tax = total * 0.10
+else:
+    tax = total * 0.05
+
+net_salary = total - tax - loan
+
+print("Net Salary:", net_salary)
