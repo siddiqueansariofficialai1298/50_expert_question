04419390f876f53d0eb524c9a598d3b9136fca8f
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:31:48 +0500

    Add files via upload

---
+++ train_reservation.py
@@ -0,0 +1,15 @@
+print("===== Train Reservation =====")
+
+seats = int(input("Available Seats: "))
+age = int(input("Age: "))
+gender = input("Gender: ").lower()
+
+if seats > 0:
+    if age >= 60:
+        print("Seat Reserved (Senior Citizen)")
+    elif gender == "female":
+        print("Seat Reserved (Ladies Quota)")
+    else:
+        print("Seat Reserved (General)")
+else:
+    print("No Seats Available")
