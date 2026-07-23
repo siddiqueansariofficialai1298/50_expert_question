0ae8aaca3f1bdb14a3877a8734e380bfff08b03f
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:19:32 +0500

    Add files via upload

---
+++ mobile_package.py
@@ -0,0 +1,12 @@
+print("===== Mobile Package =====")
+
+recharge = int(input("Recharge Amount: "))
+
+if recharge >= 5000:
+    print("Unlimited Calls + 100GB Internet")
+elif recharge >= 3000:
+    print("Unlimited Calls + 50GB Internet")
+elif recharge >= 1000:
+    print("500 Minutes + 20GB Internet")
+else:
+    print("Basic Package")
