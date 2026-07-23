10e4c8cba8aa81601f8b859c47f819bdb3e2d14b
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:18:23 +0500

    Add files via upload

---
+++ army_recuirement.py
@@ -0,0 +1,16 @@
+print("===== Army Recruitment =====")
+
+height = float(input("Height (cm): "))
+weight = float(input("Weight (kg): "))
+medical = input("Medical Passed (yes/no): ").lower()
+
+if height >= 170:
+    if weight >= 60:
+        if medical == "yes":
+            print("Selected")
+        else:
+            print("Medical Failed")
+    else:
+        print("Weight Too Low")
+else:
+    print("Height Too Low")
