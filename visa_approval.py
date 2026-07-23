6bfb95f3cf8aab83b7e06192cb61380acedabc33
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:37:54 +0500

    Add files via upload

---
+++ visa_approval.py
@@ -0,0 +1,16 @@
+print("===== Visa Approval =====")
+
+passport = input("Passport (yes/no): ").lower()
+bank = int(input("Bank Balance: "))
+criminal = input("Criminal Record (yes/no): ").lower()
+
+if passport == "yes":
+    if bank >= 1000000:
+        if criminal == "no":
+            print("Visa Approved")
+        else:
+            print("Visa Rejected (Criminal Record)")
+    else:
+        print("Insufficient Bank Balance")
+else:
+    print("Passport Missing")
