6af4c213292c2b4e015e35df52e4207df21c9596
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 16:04:58 +0500

    Add files via upload

---
+++ HR_recruitment.py
@@ -0,0 +1,22 @@
+print("===== HR RECRUITMENT =====")
+
+degree = input("Degree (yes/no): ").lower()
+experience = int(input("Experience: "))
+english = input("English Test Passed (yes/no): ").lower()
+
+if degree == "yes":
+
+    if experience >= 2:
+
+        if english == "yes":
+
+            print("Selected")
+
+        else:
+            print("English Test Failed")
+
+    else:
+        print("Experience Required")
+
+else:
+    print("Degree Required")v
