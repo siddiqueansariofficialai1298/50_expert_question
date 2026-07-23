96c43c2da41854e0f8a104e854c11da074cbc28a
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 14:02:07 +0500

    Add files via upload

---
+++ scholarship.py
@@ -0,0 +1,16 @@
+print("===== Scholarship Eligibility =====")
+
+marks = float(input("Enter Marks (%): "))
+attendance = float(input("Enter Attendance (%): "))
+income = int(input("Enter Family Income: "))
+
+if marks >= 90:
+    if attendance >= 85:
+        if income <= 50000:
+            print("🎉 Scholarship Approved")
+        else:
+            print("❌ Income Too High")
+    else:
+        print("❌ Attendance Too Low")
+else:
+    print("❌ Marks Too Low")
