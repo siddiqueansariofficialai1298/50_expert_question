10531a4632ca3386374dbf18315b8262d9c26e75
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 15:06:54 +0500

    Add files via upload

---
+++ AIRPORT_secruirty.py
@@ -0,0 +1,20 @@
+print("===== Airport Security =====")
+
+passport = input("Passport Available (yes/no): ").lower()
+ticket = input("Valid Ticket (yes/no): ").lower()
+baggage = float(input("Baggage Weight (kg): "))
+dangerous = input("Dangerous Item Found (yes/no): ").lower()
+
+if passport == "yes":
+    if ticket == "yes":
+        if baggage <= 30:
+            if dangerous == "no":
+                print("✅ Security Cleared")
+            else:
+                print("❌ Dangerous Item Detected")
+        else:
+            print("❌ Baggage Limit Exceeded")
+    else:
+        print("❌ Invalid Ticket")
+else:
+    print("❌ Passport Missing")
