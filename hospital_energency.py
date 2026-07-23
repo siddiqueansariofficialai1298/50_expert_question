4e2ae5fadf4ecbf8e47db27eab9dc0a470342ab4
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-23 14:41:52 +0500

    Add files via upload

---
+++ hospital_energency.py
@@ -0,0 +1,14 @@
+print("===== Hospital Priority =====")
+
+age = int(input("Age: "))
+emergency = input("Emergency (yes/no): ").lower()
+fever = input("Fever (yes/no): ").lower()
+
+if emergency == "yes":
+    print("Priority Level: HIGH")
+elif age >= 60:
+    print("Priority Level: HIGH")
+elif fever == "yes":
+    print("Priority Level: HIGH")
+else:
+    print("Priority Level: NORMAL")
