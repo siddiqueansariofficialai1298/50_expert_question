912bb29ca05896259caabb316172f44d0f230e45
siddiqueansariofficialai1298 <siddiqueansariofficialai@gmail.com>
2026-07-27 15:34:51 +0500

    Update fruad_detection.py

--- fruad_detection.py
+++ fruad_detection.py
@@ -1,4 +1,4 @@
-print("===== Fraud Detection =====")
+print(" Fraud Detection ")
 
 amount = int(input("Transaction Amount: "))
 country = input("Foreign Country? (yes/no): ").lower()
@@ -7,7 +7,7 @@ otp = input("OTP Verified? (yes/no): ").lower()
 if amount > 100000:
     if country == "yes":
         if otp == "no":
-            print("🚨 Fraud Alert")
+            print("Fraud Alert")
         else:
             print("Transaction Approved")
     else:
